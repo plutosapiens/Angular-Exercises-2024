aae4619476f6d363fd4aff20c0c6174e646bb6e0
Pluto <96972840+plutosapiens@users.noreply.github.com>
2024-03-10 17:02:41 +0200

    WC - Iterate themes with dynamic data

--- workshop-components/src/app/themes-list/themes-list.component.ts
+++ workshop-components/src/app/themes-list/themes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
 
 @Component({
   selector: 'app-themes-list',
@@ -8,11 +9,14 @@ import { ApiService } from '../api.service';
 })
 export class ThemesListComponent implements OnInit {
 
+  themes: Theme[] = [];
+
   constructor(private api: ApiService) {}
 
   ngOnInit(): void{
     this.api.getThemes().subscribe(themes => {
       console.log(themes)
+      this.themes = themes;
     })
   }
 }
