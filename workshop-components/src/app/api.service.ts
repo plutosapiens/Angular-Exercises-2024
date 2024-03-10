9de00ad4a82b5722a1300848ac5dbce2fcdeb303
Pluto <96972840+plutosapiens@users.noreply.github.com>
2024-03-10 14:02:31 +0200

    Fix gitignore

--- workshop-components/src/app/api.service.ts
+++ workshop-components/src/app/api.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getThemes() {
+    const { apiUrl } = environment;
+
+    return this.http.get<any>(`${apiUrl}/themes`);
+  }
+  
+  getPosts(limit?: number) {}
 }
--- workshop-components/src/app/app.module.ts
+++ workshop-components/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SharedModule } from './shared/shared.module';
 import { MainComponent } from './main/main.component';
 import { ThemesListComponent } from './themes-list/themes-list.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PostListComponent } from './post-list/post-list.component';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
