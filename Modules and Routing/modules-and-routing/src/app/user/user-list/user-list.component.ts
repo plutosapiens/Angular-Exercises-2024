295595a746ffaa3dc0e589c3a9bfa8eb188eb582
Pluto <ivet.mehmedova@gmail.com>
2024-03-19 15:36:34 +0200

    MR - Add spinner component to btn in user-list

--- Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
+++ Modules and Routing/modules-and-routing/src/app/user/user-list/user-list.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
+isLoading = false;
 
+  fetchUsers() {
+    this.isLoading = true;
+
+    setTimeout(() => {
+      this.isLoading = false
+    },4000)
+  }
 }
